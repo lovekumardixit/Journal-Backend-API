a48b2578f63f08f46edec92b2a5b0b8a130ddb15
Lav Kumar Dixit <146636723+lovekumardixit@users.noreply.github.com>
2026-05-20 12:50:44 +0530

    Update SpringSecurity.java

--- src/main/java/com/love/Backend/config/SpringSecurity.java
+++ src/main/java/com/love/Backend/config/SpringSecurity.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@Profile("dev")
 public class SpringSecurity {
 
     @Autowired
